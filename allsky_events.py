3c9b28b75213497acb60b9b20a2e8d9833aab292
pquigle <pquigle@uwo.com>
2022-11-15 10:54:35 -0500

    Minor updates

--- allsky_events.py
+++ allsky_events.py
@@ -16,7 +16,8 @@ import numpy as np
 ###########################
 
 evdate = input("Event date (YYYYMMDD): ")
-evpath = f"../../FLIR-Data/{evdate}/evmags{evdate}.log"
+#evpath = f"../../FLIR-Data/{evdate}/evmags{evdate}.log"
+evpath = f"data/{evdate}/evmags{evdate}.log"
 
 evlist = np.loadtxt(evpath,dtype=object)[:,[0,10]]
 evlist[:,0] = [ev[12:18] for ev in evlist[:,0]]
@@ -24,7 +25,8 @@ evlist[:,0] = [ev[12:18] for ev in evlist[:,0]]
 unique_ev = evlist[np.unique(evlist[:,0],return_index=True)[1]]
 unique_ev = unique_ev[unique_ev[:,1].argsort()]
 
-np.savetxt(f"../../FLIR-Data/{evdate}/UniqueEvents{evdate}.log",
+#np.savetxt(f"../../FLIR-Data/{evdate}/UniqueEvents{evdate}.log",
+np.savetxt(f"data/{evdate}/UniqueEvents{evdate}.log",
            unique_ev[::-1],fmt="%.6s",
            delimiter=",")
 
